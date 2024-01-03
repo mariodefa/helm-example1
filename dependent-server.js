85d6f3e829833a1088b1d8aa7c9eb045a05265d3
Mario Rojo <marioruiz.rojo@defa.com>
2024-01-03 13:22:14 +0100

    three attempts, clusterip, readiness mysubchart

--- dependent-server.js
+++ dependent-server.js
@@ -13,23 +13,50 @@ let dessertDisplay = dessert1;
 let drink2Display = 'error';
 let dessert2Display = 'error';
 
-async function init(){
-    if (DEPENDENT) {
-        try {
-            const response = await axios.get(process.env.URL_DEPENDENCY);
-            drink2Display = response.data.drink2;
-            dessert2Display = response.data.dessert2;
-        } catch (error) {
-            console.error(`Error fetching from ${process.env.URL_DEPENDENCY}:`, error);
-        }
-    }
-    else{
-        drinkDisplay = drink2;
-        dessertDisplay = dessert2;
-        drink2Display = '';
-        dessert2Display = '';
+//----------------FUNCTIONS---------------------
+
+async function requestSecondChoices(attempts) {
+    try {
+        const response = await axios.get(process.env.URL_DEPENDENCY);
+        drink2Display = response.data.drinkDisplay;
+        dessert2Display = response.data.dessertDisplay;
+        console.log('success '+ response.data);
+    } catch (error) {
+        console.error(`Attemp to reach ${process.env.URL_DEPENDENCY} number:`, attempts);
+        throw error; // Lanzamos el error para que pueda ser capturado en init()
     }
 }
+
+function noSecondChoices() {
+    drinkDisplay = drink2;
+    dessertDisplay = dessert2;
+    drink2Display = '';
+    dessert2Display = '';
+}
+
+async function init(){
+    let attempts = 0;
+    while (attempts < 3) {
+        try {
+            if (DEPENDENT) {
+                await requestSecondChoices(attempts);
+                break;
+            } else {
+                noSecondChoices();
+                break;
+            }
+        } catch (error) {
+            attempts++;
+            if (attempts < 3) {
+                console.log(`Attempt ${attempts} failed. Retrying in 3 seconds...`);
+                await new Promise(resolve => setTimeout(resolve, 3000));
+            } else {
+                console.log(`Attempt ${attempts} failed. No more retries.`);
+            }
+        }
+    }
+}
+//-------------------------------------
 
 init();
 
@@ -42,5 +69,5 @@ app.get('/json', (req, res) => {
 });
 
 app.listen(port, '0.0.0.0', () => {
-  console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://0.0.0.0:${port}`);
 });
